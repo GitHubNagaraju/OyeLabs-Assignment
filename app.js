bf5c03c6e49e852f7b81f05d0f9816c73528a665
Nagaraju Ankam <nagarajuankam6@gmail.com>
2023-06-27 19:32:27 +0000

    cmit message

---
+++ app.js
@@ -0,0 +1,31 @@
+const { insertCustomers } = require("./3rd_promise.js");
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const path = require("path");
+let db = null;
+
+const dbPath = path.join(__dirname, "todoApplication.db");
+
+const connectDbServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server running at http://localhost:3000");
+    });
+  } catch (e) {
+    console.log(`DB Error:${e.message}`);
+    process.exit(1);
+  }
+};
+
+connectDbServer();
+
+app.post("/",insertCustomers}
